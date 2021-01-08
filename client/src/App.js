ef99c53bc86a617664ecf75d1dfba9d3e9054241
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-08 11:41:51 -0800

    added input component and style folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-
+import SearchInput from "./components/SearchInput";
 function App() {
   return (
     <div className="App">
+      <h2>The Shoppies</h2>
+      <SearchInput />
     </div>
   );
 }
---
+++ client/src/components/SearchInput.js
@@ -0,0 +1,17 @@
+import searchIcon from "../assets/Icons/Icon-search.svg";
+
+export default function SearchInput() {
+  return (
+    <form className="title-search__form">
+      <h3 className="title-search__form--title">Movie title</h3>
+      <div>
+        <input type="text" className="title-search__form--input" />
+        <img
+          alt="movie title search icon"
+          src={searchIcon}
+          className="title-search__form--icon"
+        />
+      </div>
+    </form>
+  );
+}
