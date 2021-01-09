fb382c0602021da8f43a253434b0e20e50aaaa20
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-08 18:34:15 -0800

    added reactstrap button to nominateditem

--- client/src/components/NominatedItem.js
+++ client/src/components/NominatedItem.js
@@ -1,12 +1,13 @@
+import { Button } from "reactstrap";
 export default function NominatedItem({ movie, handleRemove }) {
   return (
     <li className="list-item">
       <p className="list-item__title">
         {movie.Title} ({movie.Year})
       </p>
-      <button className="list-item__button" onClick={handleRemove}>
+      <Button color ="info" className="list-item__button" onClick={handleRemove}>
         Remove
-      </button>
+      </Button>
     </li>
   );
 }
