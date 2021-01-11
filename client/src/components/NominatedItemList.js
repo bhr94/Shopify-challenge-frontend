fbc0958f4a3c6d434bc47bf46c380046e6eda52b
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-11 11:33:46 -0800

    add a check if movies exist in localstorage

--- client/src/components/NominatedItemList.js
+++ client/src/components/NominatedItemList.js
@@ -6,11 +6,11 @@ export default function NominatedItemList({ handleRemove }) {
   return (
     <section className="list-container nominated-list">
       <h4>Nominations</h4>
-      {movies.length === 5 ? (
+      {movies&&movies.length === 5 ? (
         <Alert>Congratualtions!!! Now you have 5 nominations</Alert>
       ) : null}
       <ul className="list-container__list nomination-list">
-        {movies.length > 0 &&
+        {movies&&movies.length > 0 &&
           movies.map((movie) => {
             return (
               <NominatedItem
